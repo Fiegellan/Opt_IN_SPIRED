49f5746fbbc4c9e515ee18e215821ce2ac1b45f2
Fiegellan <cfiegel@horizon.csueastbay.edu>
2017-05-28 21:32:38 -0700

    Initial Commit

---
+++ Optinote/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Optinote
+//
+//  Created by Christopher Fiegel on 5/28/17.
+//  Copyright © 2017 Optimi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
